b87119f3fd565cb976b8d640c81f49507ec55841
hanstar <114556143+shinebyul@users.noreply.github.com>
2024-07-30 21:27:45 +0900

    [Gold V] Title: 사다리 타기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/2469. 사다리 타기/사다리 타기.cc
+++ 백준/Gold/2469. 사다리 타기/사다리 타기.cc
@@ -1,109 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int min(int a, int b) {
-    return a < b ? a : b;
-}
+int min(int a, int b);
 
-int main() {
+int main(){
+
     int k, n;
-    scanf("%d", &k);
-    scanf("%d", &n);
+    scanf("%d",&k);
+    scanf("%d",&n);
     getchar();
 
-    // 원하는 결과값 입력받기
-    char *result = (char *)malloc((k + 1) * sizeof(char));
-    for (int i = 0; i < k; i++) {
-        scanf("%c", &result[i]);
+    //원하는 결과값 입력받기
+    char *result = (char*)malloc(k*sizeof(char));
+    for(int i=0;i<k;i++){
+        scanf("%c",&result[i]);
     }
-    result[k] = '\0';
     getchar();
+
 
     // 사다리 입력받기
-    int hide = -1;
-    char **ladder = (char **)malloc(n * sizeof(char *));
-    for (int i = 0; i < n; i++) {
-        ladder[i] = (char *)malloc((k - 1) * sizeof(char));
+    int hide;
+    char **ladder = (char**)malloc(n*sizeof(char*));
+    for(int i=0;i<n;i++){
+        ladder[i]=(char*)malloc((k-1)*sizeof(char));
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < k - 1; j++) {
-            scanf("%c", &ladder[i][j]);
-            if (ladder[i][j] == '?') hide = i;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<k-1;j++){
+            scanf("%c",&ladder[i][j]);
+            if(ladder[i][j]=='?') hide=i;
         }
         getchar();
     }
 
-    int *up = (int *)malloc(k * sizeof(int));
-    int *down = (int *)malloc(k * sizeof(int));
 
-    for (int i = 0; i < k; i++) {
-        up[i] = i;
+    int *up = (int*)malloc(k*sizeof(int));
+    int *down = (int*)malloc(k*sizeof(int));
+
+    for(int i=0;i<k;i++){
+        up[i]=i;
     }
 
-    // 위에서 아래로 내려오기
-    for (int i = 0; i < hide; i++) {
-        for (int j = 0; j < k - 1; j++) {
-            if (ladder[i][j] == '-') {
-                int temp = up[j];
-                up[j] = up[j + 1];
-                up[j + 1] = temp;
+    // printf("hide : %d\n", hide);
+
+    //위에꺼 아래로 내리기
+    for(int i=0;i<k;i++){
+        for(int j=0;j<hide;j++){
+            if(up[i]!=0 && ladder[j][up[i]-1]=='-'){
+                up[i]-=1;
+            }else if(up[i]!=k-1 && ladder[j][up[i]]=='-'){
+                up[i]+=1;
             }
         }
     }
 
-    // 결과를 down 배열에 저장
-    for (int i = 0; i < k; i++) {
-        down[i] = result[i] - 'A';
+    //result를 down으로 초기화
+    for(int i=0;i<k;i++){
+        down[result[i]-'A']=i;
     }
 
-    // 아래에서 위로 올라오기
-    for (int i = n - 1; i > hide; i--) {
-        for (int j = 0; j < k - 1; j++) {
-            if (ladder[i][j] == '-') {
-                int temp = down[j];
-                down[j] = down[j + 1];
-                down[j + 1] = temp;
+
+    //아래꺼 위로 올리기
+    for(int i=0;i<k;i++){
+        for(int j=n-1;j>hide;j--){
+            if(down[i]!=0 && ladder[j][down[i]-1]=='-'){
+                down[i]-=1;
+            }else if(down[i]!=k-1 && ladder[j][down[i]]=='-'){
+                down[i]+=1;
             }
         }
     }
 
-    char *ret = (char *)malloc((k - 1) * sizeof(char));
-    for (int i = 0; i < k - 1; i++) {
-        ret[i] = '*';
+    char *ret = (char*)malloc((k-1)*sizeof(char));
+    for(int i=0;i<k-1;i++){
+        ret[i]='*';
     }
 
-    for (int i = 0; i < k - 1; i++) {
-        if (up[i] == down[i + 1] && up[i + 1] == down[i]) {
-            ret[i] = '-';
-            int temp = up[i];
-            up[i] = up[i + 1];
-            up[i + 1] = temp;
-        }
-    }
-
-    for (int i = 0; i < k - 1; i++) {
-        if (up[i] != down[i]) {
-            for (int j = 0; j < k - 1; j++) {
-                ret[j] = 'x';
+int m;
+    for(int i=0; i<k;i++){
+        if(up[i]-down[i]==1 || up[i]-down[i]==-1){
+            m= min(up[i],down[i]);
+            if(ret[m-1]=='-' || ret[m+1]=='-'){
+                for(int i=0;i<k-1;i++){
+                    printf("x");
+                }
+                return 0;
             }
-            break;
+            ret[m]='-';
+        }else if(up[i]!=down[i]){
+            for(int i=0;i<k-1;i++){
+                printf("x");
+            }
+            return 0;
         }
     }
 
-    for (int i = 0; i < k - 1; i++) {
-        printf("%c", ret[i]);
+    for(int i=0;i<k-1;i++){
+        printf("%c",ret[i]);
     }
-    printf("\n");
-
-    free(result);
-    for (int i = 0; i < n; i++) {
-        free(ladder[i]);
-    }
-    free(ladder);
-    free(up);
-    free(down);
-    free(ret);
 
     return 0;
 }
+
+
+int min(int a, int b){
+    if(a<=b) return a;
+    return b;
+}
+
