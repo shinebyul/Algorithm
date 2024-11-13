6a630aa39eaab9c8a708012923cf499106a1a780
hanstar <114556143+shinebyul@users.noreply.github.com>
2024-11-14 02:48:16 +0900

    [level 2] Title: 요격 시스템, Time: 414.90 ms, Memory: 190 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,25 +1,35 @@
 import java.util.*;
 
 class Solution {
-
+    
     public int solution(int[][] targets) {
-        // 1. 종료 지점을 기준으로 오름차순 정렬
-        Arrays.sort(targets, Comparator.comparingInt(a -> a[1]));
-
         int answer = 0;
-        double lastIntercept = -1;  // 마지막 요격 미사일의 x 좌표 (최초에는 아무것도 요격하지 않은 상태)
-
-        for (int[] target : targets) {
-            int start = target[0];
-            int end = target[1];
-
-            // 현재 요격 미사일의 위치가 현재 미사일의 범위를 커버하지 못하면 새로운 요격 미사일 필요
-            if (lastIntercept < start) {
-                answer++;  // 요격 미사일 하나 추가
-                lastIntercept = end - 0.5;  // 요격 지점을 end - 0.5로 설정하여 개구간 요건 충족
+        
+        // 1. 오름차순
+        // Arrays.sort(targets, Comparator.comparingInt(a -> a[0]));
+        Arrays.sort(targets, Comparator.comparingInt(a -> a[1]));
+        
+        // 2. 하나씩 꺼내서 삭제
+        answer = yogeuk(targets);
+        
+        return answer;
+    }
+    
+    private int yogeuk(int[][] targets){
+        int count = 0;
+        
+        int[] f = targets[0];
+        count++;
+        
+        for(int i=1;i<targets.length;i++){
+            int[] s = targets[i];
+            
+            if(s[0]>=f[1]){
+                f=targets[i];
+                count++;
             }
         }
-
-        return answer;
+        
+        return count;
     }
 }
