d81b8e952c24ea85d463cc8a32a1cdd7a0658ad2
hanstar <114556143+shinebyul@users.noreply.github.com>
2024-11-21 00:08:29 +0900

    [level 2] Title: 광물 캐기, Time: 259.93 ms, Memory: 284 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    
+    static int result = Integer.MAX_VALUE;
+    
+    public int solution(int[] picks, String[] minerals)     {
+        
+        dfs(picks, minerals, 0, 0);
+        return result;
+    }
+         
+    
+    public static void dfs(int[] picks, String[] minerals, int depth, int stress){
+        if(depth == minerals.length || Arrays.stream(picks).sum()==0){
+            result = Math.min(result,stress);
+            return;
+        }
+        
+        for(int i=0;i<3; i++){
+            if(picks[i]>0){
+                picks[i]--;
+                int sum = stress;
+                for(int j=depth; j<Math.min(depth+5, minerals.length);j++){
+                    switch(i){
+                        case 0:
+                            sum +=1;
+                            break;
+                        case 1:
+                            if(minerals[j].equals("diamond"))
+                                sum+=5;
+                            else
+                                sum+=1;
+                            break;
+                        case 2:
+                            if(minerals[j].equals("diamond"))
+                                sum+=25;
+                            else if(minerals[j].equals("iron"))
+                                    sum+=5;
+                            else
+                                sum+=1;
+                            break;
+                            
+                    }
+                }
+                dfs(picks, minerals, depth+5, sum);
+                picks[i]++;
+            }
+        }
+    }
+}
