7927b621e3d7b2c07bd0f3cf7521b0d55979a834
hanstar <114556143+shinebyul@users.noreply.github.com>
2025-02-03 18:05:00 +0900

    [level 2] Title: 모음 사전, Time: 14.29 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,26 @@
 class Solution {
-    public String[] dict;
-    public int cnt = 0, answer = 0;
+    String[] dict = {"A","E","I","O","U"};
+    int answer = 0;
+    int cnt = 0;
+    
     public int solution(String word) {
-        dict = "AEIOU".split("");
+        
         dfs("", word);
         
         return answer;
     }
     
-    public void dfs(String cur, String word){
-        if(cur.length() == 5 || cur.equals(word)){
-            if(cur.equals(word)){
-                answer = cnt;
-            }
+    private void dfs(String current, String word){
+        if(current.equals(word)){
+            answer = cnt;
             return;
         }
-        
-        for(int i = 0; i < 5; i++){
+        if(current.length() >= 5){
+            return;
+        }
+        for(int i=0;i<5;i++){
             cnt++;
-            dfs(cur+dict[i],word);
+            dfs(current+dict[i], word);
         }
     }
 }
