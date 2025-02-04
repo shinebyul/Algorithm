2c466be5ae1c9003877f54bfda00ad077c35895e
hanstar <114556143+shinebyul@users.noreply.github.com>
2025-02-05 00:21:06 +0900

    [level 2] Title: 타겟 넘버, Time: 7.50 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,16 @@
-
-import java.util.*;
 class Solution {
-    int answer = 0;
     public int solution(int[] numbers, int target) {
-        
-        bfs(numbers, target);
+        int answer = 0;
+        answer = dfs(numbers, target, 0, 0);
         return answer;
     }
-    void bfs(int[] numbers, int target){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        
-        for(int i=0;i<numbers.length;i++){
-            for(int j=0;j<Math.pow(2,i);j++){
-                int n = queue.poll();
-                queue.add(n+numbers[i]);
-                queue.add(n-numbers[i]);
-            }
+    
+    int dfs(int[] numbers, int target, int depth, int n){
+        if(depth==numbers.length){
+            if(n==target) return 1;
+            return 0;
         }
         
-        while(!queue.isEmpty()){
-            if(queue.poll()==target) answer++;
-        }
+        return dfs(numbers, target, depth+1, n+numbers[depth]) + dfs(numbers, target, depth+1, n-numbers[depth]);
     }
 }
